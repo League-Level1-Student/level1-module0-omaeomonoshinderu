97de35c1900795dcfc862e75d4537542a6dcb77e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-11-14 13:28:54 -0800

    houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,16 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	
 	public void run() {
-		
+	Robot rob = new Robot("mini");
+	//rob.penDown();
+	rob.setSpeed(100);
+	rob.move(-280);
+	rob.turn(-90);
+	rob.move(450);
+	rob.turn(-180);
 	}
 }
