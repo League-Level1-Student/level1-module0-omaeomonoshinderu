c7e5353789d30aa7e7481393a017573b44b00ef9
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-12-05 13:27:57 -0800

    houses unfinished

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -6,11 +6,15 @@ public class Houses {
 	
 	public void run() {
 	Robot rob = new Robot("mini");
-	//rob.penDown();
 	rob.setSpeed(100);
 	rob.move(-280);
 	rob.turn(-90);
 	rob.move(450);
 	rob.turn(-180);
+	rob.penDown();
+	rob.setPenColor(3, 252, 28);
+	rob.move(30);
+	
+	
 	}
 }
